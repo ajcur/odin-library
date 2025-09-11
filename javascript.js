1bb695c0798b32e393524bb7f07b9bf20181f308
ajcur <173679835+ajcur@users.noreply.github.com>
2025-09-11 10:47:22 +0200

    Implement dialog box when clicking Add Book

--- javascript.js
+++ javascript.js
@@ -12,8 +12,20 @@ function addBookToLibrary(title, author, pagecount, read) {
   myLibrary.push(newBook);
 }
 
-addBookToLibrary('A', 'BB', 102, true);
+const newBookButton = document.querySelector('#add-book');
+const newBookDialog = document.querySelector('.new-book-window');
+const addButton = document.querySelector('.add-button');
+
+newBookButton.addEventListener('click', () => {
+    newBookDialog.showModal();
+})
+
+addButton.addEventListener('click', () => {
+    newBookDialog.close();
+})
+
+/* addBookToLibrary('A', 'BB', 102, true);
 
 addBookToLibrary('C', 'DD', 144, false);
 
-console.log(myLibrary);
+console.log(myLibrary); */
