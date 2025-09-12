fdbb7276b72aa3bb3183998f5e7992c8bc790ad1
ajcur <173679835+ajcur@users.noreply.github.com>
2025-09-12 11:16:00 +0200

    Implement Mark Read and Delete buttons, add more styling

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ const pagecountField = document.querySelector('#pagecount');
 const readCheckbox = document.querySelector('#read');
 const bookshelfBox = document.querySelector('.bookshelf-box');
 
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(id, title, author, pagecount, read) {
     this.id = id;
@@ -20,9 +20,36 @@ function Book(id, title, author, pagecount, read) {
 function addBookToLibrary(id, title, author, pagecount, read) {
     let newBook = new Book(id, title, author, pagecount, read);
     myLibrary.push(newBook);
+    updateBookDisplays();
+}
+
+function deleteBook(book) {
+    i = myLibrary.indexOf(book);
+    myLibrary = myLibrary.filter((item) => {
+        return myLibrary.indexOf(item) != i;
+    })
+    updateBookDisplays();
+}
+
+function updateBookDisplays() {
     bookshelfBox.replaceChildren();
     myLibrary.forEach(addBookDisplay);
 }
+
+function switchReadStatus(book) {
+    if (book.read) {
+        book.read = false;
+    } else book.read = true;
+}
+
+function displayReadStatus(book, readDisplay, markReadButton) {
+    if (book.read) {
+        readDisplay.textContent = '☑️ Read';
+        markReadButton.textContent = 'Mark Unread';
+    } else {
+        readDisplay.textContent = '❌ Not read';
+        markReadButton.textContent = 'Mark Read';
+}}
 
 function resetDialog() {
     titleField.value = '';
@@ -33,24 +60,47 @@ function resetDialog() {
 
 function addBookDisplay(book) {
     let bookDisplay = document.createElement('div');
-    bookDisplay.classList = 'book-display';
     let titleDisplay = document.createElement('p');
-    titleDisplay.classList = 'title-display';
-    titleDisplay.textContent = `${book.title}`;
-    bookDisplay.appendChild(titleDisplay);
     let authorDisplay = document.createElement('p');
-    authorDisplay.classList = 'author-display';
-    authorDisplay.textContent = `By ${book.author}`;
-    bookDisplay.appendChild(authorDisplay);
     let pagecountDisplay = document.createElement('p');
-    pagecountDisplay.classList = 'pagecount-display';
-    pagecountDisplay.textContent = `${book.pagecount} pages`;
-    bookDisplay.appendChild(pagecountDisplay);
     let readDisplay = document.createElement('p');
-    if (book.read) {
-        readDisplay.textContent = '☑️ Read';
-    } else readDisplay.textContent = '❌ Not read';
+    let buttonsBox = document.createElement('div');
+    let markReadButton = document.createElement('button');
+    let deleteBookButton = document.createElement('button');
+
+    bookDisplay.classList = 'book-display';
+    titleDisplay.classList = 'title-display';
+    authorDisplay.classList = 'author-display';
+    pagecountDisplay.classList = 'pagecount-display'
+    readDisplay.classList = 'read-display';
+    buttonsBox.classList = 'buttons-box';
+    markReadButton.classList = 'mark-read';
+    deleteBookButton.classList = 'delete-book';
+    
+    titleDisplay.textContent = `${book.title}`;
+    authorDisplay.textContent = `By ${book.author}`;
+    pagecountDisplay.textContent = `${book.pagecount} pages`;
+    deleteBookButton.textContent = 'Delete Book';
+
+    displayReadStatus(book, readDisplay, markReadButton);
+
+    markReadButton.addEventListener('click', () => {
+        switchReadStatus(book);
+        displayReadStatus(book, readDisplay, markReadButton);
+    })
+    deleteBookButton.addEventListener('click', () => {
+        deleteBook(book);
+    })
+
+    buttonsBox.appendChild(markReadButton);
+    buttonsBox.appendChild(deleteBookButton);
+
+    bookDisplay.appendChild(titleDisplay);
+    bookDisplay.appendChild(authorDisplay);;
+    bookDisplay.appendChild(pagecountDisplay);
     bookDisplay.appendChild(readDisplay);
+    bookDisplay.appendChild(buttonsBox);
+
     bookshelfBox.appendChild(bookDisplay);
 }
 
